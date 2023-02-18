48f8f8f385859d59b41f4d4538b9f312675a5b07
Ammarhussain110 <ammarhussain051@gmail.com>
2023-02-18 07:54:22 +0500

    arrays

---
+++ bascis_chapter_1/array.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string book_names[3] = {"chemistary", "maths", "physics"};
+    int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+//    cout << book_names[2] << endl;
+    for (int i = 0; i < 3; i++)
+    {
+      cout << book_names[i] << endl;
+    }
+    // cout << book_names << endl;
+
+    return 0;
+}
